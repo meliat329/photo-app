820146bfc96000ae8a2bf3d9ed3bb94305cd9978
meliat329 <33754787+meliat329@users.noreply.github.com>
2022-02-27 14:33:08 -0600

    Revert "lab7"
    This reverts commit aafefa245d5abfc34c7190be44e897412686da03.

--- app.py
+++ app.py
@@ -4,13 +4,10 @@ from flask import Flask, request
 from flask_restful import Api
 from flask_cors import CORS
 from flask import render_template
-import flask_jwt_extended  
-import decorators
 import os
 from models import db, User, ApiNavigator
 from views import bookmarks, comments, followers, following, \
     posts, profile, stories, suggestions, post_likes
-from views import authentication, token
 
 
 
@@ -68,33 +65,7 @@ def api_docs():
         url_root=request.url_root[0:-1] # trim trailing slash
     )
 
-#JWT config variables and manager (add after app object created):
-app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
-app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies"]
-app.config["JWT_COOKIE_SECURE"] = False
-jwt = flask_jwt_extended.JWTManager(app)
 
-# Initialize routes of 2 new views
-authentication.initialize_routes(app)
-token.initialize_routes(api)
-
-
-# Updated API endpoint includes a reference to 
-# access_token and csrf token.
-@app.route('/api')
-@decorators.jwt_or_login
-def api_docs():
-    access_token = request.cookies.get('access_token_cookie')
-    csrf = request.cookies.get('csrf_access_token')
-    navigator = ApiNavigator(flask_jwt_extended.current_user)
-    return render_template(
-        'api/api-docs.html', 
-        user=app.current_user,  #TODO: change to flask_jwt_extended.current_user
-        endpoints=navigator.get_endpoints(),
-        access_token=access_token,
-        csrf=csrf,
-        url_root=request.url_root[0:-1] # trim trailing slash
-    )
 
 # enables flask app to run using "python3 app.py"
 if __name__ == '__main__':
